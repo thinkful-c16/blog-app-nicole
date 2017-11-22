674087677b5336de605d86d56d9ab2867c1a74c2
Nicole Gilbert <nicolegilbert@Nicoles-MacBook-Air.local>
2017-11-22 08:20:33 -0800

    updated config.js with db connection info

--- config.js
+++ config.js
@@ -1,3 +1,14 @@
 'use strict';
 
+const DATABASE_URL = process.env.DATABASE_URL
+|| globa.DATABASE_URL
+|| 'postgresql://localhost/blog-app' //need to have the right URL in place
+
+exports.DATABASE = {
+    client: 'pg',
+    connection: DATABASE_URL,
+    pool: {min: 0, max: 3}, // Fixes issue with Elephant SQL 
+    debug: true //outputs knex debugging information
+}
+
 exports.PORT = process.env.PORT || 8080; 
